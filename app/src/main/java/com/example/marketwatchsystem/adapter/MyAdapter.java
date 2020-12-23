6d9687417ecb27af1b5e8a7c21b8041225b4a99d
Rofiquldk1 <63155761+Rofiquldk1@users.noreply.github.com>
2020-12-24 01:02:41 +0600

    Commit

---
+++ app/src/main/java/com/example/marketwatchsystem/adapter/MyAdapter.java
@@ -0,0 +1,60 @@
+package com.example.marketwatchsystem.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.marketwatchsystem.Model.ProductInfo;
+import com.example.marketwatchsystem.R;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<ProductInfo> products = new ArrayList<>();
+    private Context context;
+
+    public MyAdapter(ArrayList<ProductInfo> products, Context context) {
+        this.products = products;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+
+    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_products,parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+        holder.name.setText(products.get(position).getProductName().toString());
+        holder.code.setText(products.get(position).getProductCode().toString());
+        holder.message.setText(products.get(position).getMessage().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name, code,message;
+        public ViewHolder(View view) {
+            super(view);
+
+            name = view.findViewById(R.id.text_name);
+            code = view.findViewById(R.id.text_code);
+            message = view.findViewById(R.id.text_msg);
+        }
+    }
+}
